72e662e678f6759c56ef02d168bda07340d3e311
Oliver Franke <oliver.franke@limebit.de>
2021-11-25 17:46:27 +0100

    DIG-163 create prediction

---
+++ src/predict.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import pickle
+
+# load trained model
+file_to_open = open("data/models/baummethoden_lr.pickle", 'rb')
+trained_model = pickle.load(file_to_open)
+file_to_open.close()
+
+# load data that we want predictions for
+prediction_data = pd.read_csv('data/prediction-data.csv', sep=";")
+
+print(trained_model.predict(prediction_data))
