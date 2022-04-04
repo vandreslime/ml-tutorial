036e6fa5461f33fa3e134c0ab3d09f4e6a9dacd2
Oliver Franke <oliver.franke@limebit.de>
2022-04-04 16:43:17 +0300

    change hello_world api endpoint

--- src/api.py
+++ src/api.py
@@ -22,7 +22,7 @@ def index():
 
 @app.route("/hello_world", methods=["GET"])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello World!</p>"
 
 
 @app.route("/training_data", methods=["GET"])
